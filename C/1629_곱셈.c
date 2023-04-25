a649ed70b26440f37950d56f215da5d7cbff510d
YUN <102664109+YJH2848@users.noreply.github.com>
2023-04-25 23:44:03 +0900

    장차 6시간동안 푼 문제

---
+++ C/1629_곱셈.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+unsigned long long int a, b, c, save;
+
+unsigned long long int calc(unsigned long long int A, unsigned long long int B){
+	if(B == 1){
+	return A % c;
+	} 
+	unsigned long long int result = calc(A, B/2);
+	result = (result * result) % c;
+	if (B % 2 == 0) {
+    	return result;
+	} else {
+    	return (result * A) % c;
+	}
+}
+
+int main() {
+	scanf("%llu %llu %llu", &a, &b, &c);
+	save = calc(a, b);
+	if(a == c){
+		 printf("0");
+		 return 0;
+	} else if(b == 1){
+		printf("%llu", a % c);
+	} else {
+		printf("%llu", save);
+	}
+	return 0;
+}
